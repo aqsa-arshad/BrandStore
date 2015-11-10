90efc74508e79983e646e46ea540bf0b5d24694b
shehriyar-zafar-confiz <shehriyar.zafar@confiz.com>
2015-11-10 15:32:54 +0500

    Work on the order detail page
    Test Plan: Na
    
    Subscribers: omair.sajid
    
    Differential Revision: http://phabricator.confiz.com/D5668

---
+++ Web/OrderDetail.aspx.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using AspDotNetStorefront;
+using AspDotNetStorefrontCore;
+
+public partial class OrderDetail : SkinBase
+{
+    public int orderNumber;
+    private string RecurringSubscriptionID = string.Empty;
+    public IDataReader reader;
+    public string m_StoreLoc = AppLogic.GetStoreHTTPLocation(true);
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        RequireSecurePage();
+        orderNumber = CommonLogic.QueryStringUSInt("OrderNumber");
+        GetOrderInfo();
+        GetOrderItemsDetail();
+    }
+
+    void GetOrderInfo()
+    {
+        using (var conn = DB.dbConn())
+        {
+            conn.Open();
+            using (var cmd = new SqlCommand("aspdnsf_GetOrderDetail", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@ORDERNUMBER", orderNumber);
+
+                reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    lblOrderNumber.Text = reader["OrderNumber"].ToString();
+                    lblOrderDate.Text = reader["OrderDate"].ToString();
+                    //Billing Address
+                    lblBAFullName.Text = reader["BillingFirstName"].ToString() + ' ' + reader["BillingLastName"].ToString();
+                    lblBACompany.Text = reader["BillingCompany"].ToString();
+                    lblBAAddress1.Text = reader["BillingAddress1"].ToString();
+                    lblBAAddress2.Text = reader["BillingAddress2"].ToString();
+                    lblBASuite.Text = reader["BillingSuite"].ToString();
+                    lblBACityStateZip.Text = reader["BillingCity"] + ", " + reader["BillingState"] + ' ' + reader["BillingZip"];
+                    lblBACountry.Text = reader["BillingCountry"].ToString();
+                    lblBAPhone.Text = reader["BillingPhone"].ToString();
+                    //Shipping Address
+                    lblSAFullName.Text = reader["ShippingFirstName"].ToString() + ' ' + reader["ShippingLastName"].ToString();
+                    lblSACompany.Text = reader["ShippingCompany"].ToString();
+                    lblSAAddress1.Text = reader["ShippingAddress1"].ToString();
+                    lblSAAddress2.Text = reader["ShippingAddress2"].ToString();
+                    lblSASuite.Text = reader["ShippingSuite"].ToString();
+                    lblSACityStateZip.Text = reader["ShippingCity"] + ", " + reader["ShippingState"] + ' ' + reader["ShippingZip"];
+                    lblSACountry.Text = reader["ShippingCountry"].ToString();
+                    lblSAPhone.Text = reader["ShippingPhone"].ToString();
+                    //Payment Methods
+                    lblPMCardInfo.Text = reader["CardType"].ToString() + ' ' + (string.Concat("*********", reader["CardNumber"].ToString()));
+                    lblPMExpireDate.Text = "Expires: " + reader["CardExpirationMonth"].ToString() + '/' + reader["CardExpirationYear"].ToString();
+                    lblPMCountry.Text = reader["BillingCountry"].ToString();
+                    //Billing Amounts
+                    lblSubTotal.Text = reader["OrderSubtotal"].ToString();
+                    lblTax.Text = reader["OrderTax"].ToString();
+                    lblShippingCost.Text = reader["OrderShippingCosts"].ToString();
+                    lblTotalAmount.Text = reader["OrderTotal"].ToString();
+                    RecurringSubscriptionID = reader["RecurringSubscriptionID"].ToString();
+                }
+                conn.Close();
+            }
+        }
+    }
+
+    private void GetOrderItemsDetail()
+    {
+        using (var conn = DB.dbConn())
+        {
+            conn.Open();
+            using (var cmd = new SqlCommand("aspdnsf_GetOrderItemsDetail", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@ORDERNUMBER", orderNumber);
+
+                reader = cmd.ExecuteReader();
+                //TODO: It 'll be removed after showing the product image
+                //if (reader.Read())
+                //{
+                //    do
+                //    {
+                //        imgProduct.ImageUrl = AppLogic.LookupImage("Product", int.Parse(reader["ProductID"].ToString()),
+                //            reader["ImageFileNameOverride"].ToString(), reader["SKU"].ToString(), "icon",
+                //            ThisCustomer.SkinID,
+                //            ThisCustomer.LocaleSetting);
+                //        lblProductName.Text = reader["OrderedProductName"].ToString();
+                //        lblProductID.Text = reader["ProductID"].ToString();
+                //        lblDescription.Text = reader["Description"].ToString();
+                //        lblQuantity.Text = reader["Quantity"].ToString();
+                //        lblDelivery.Text = reader["ShippingMethod"].ToString();
+                //    } while (reader.Read());
+                //}
+
+                rptOrderItemsDetail.DataSource = reader;
+                rptOrderItemsDetail.DataBind();  
+                conn.Close();
+            }
+        }
+    }
+
+    public string GetReorder()
+    {
+        if (RecurringSubscriptionID.Length == 0 && AppLogic.AppConfigBool("Reorder.Enabled"))
+        {            
+            return "<a class=\"btn btn-primary btn-half\" role=\"button\" href=\"javascript:ReOrder(" + orderNumber + ");\">" + "Print Receipt" + "</a>";
+        }
+        else
+        {
+            return String.Empty;
+        }
+    }
+
+    protected override string OverrideTemplate()
+    {
+        var masterHome = AppLogic.HomeTemplate();
+        if (masterHome.Trim().Length == 0)
+        {
+            masterHome = "JeldWenTemplate";
+        }
+        if (masterHome.EndsWith(".ascx"))
+        {
+            masterHome = masterHome.Replace(".ascx", ".master");
+        }
+        if (!masterHome.EndsWith(".master", StringComparison.OrdinalIgnoreCase))
+        {
+            masterHome = masterHome + ".master";
+        }
+        if (!CommonLogic.FileExists(CommonLogic.SafeMapPath("~/App_Templates/Skin_" + SkinID + "/" + masterHome)))
+        {
+            masterHome = "JeldWenTemplate";
+        }
+        return masterHome;
+    }
+}
--- Web/orderhistory.aspx.cs
+++ Web/orderhistory.aspx.cs
@@ -8,7 +8,6 @@ using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Globalization;
-using System.Web;
 using System.Data;
 using System.Web.UI.WebControls;
 using AspDotNetStorefrontCore;
