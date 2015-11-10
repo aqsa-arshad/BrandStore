e1f5de6d645c9c7519d8bf922d10485719c3c0ee
aqsa-arshad-confiz <aqsa.arshad@confiz.com>
2015-11-10 03:52:31 -0800

    Complete Customer Alert module Functionality

--- Web/App_Templates/Skin_3/JeldWenTemplate.master.cs
+++ Web/App_Templates/Skin_3/JeldWenTemplate.master.cs
@@ -1,7 +1,12 @@
-﻿using System.Activities.Expressions;
-using AspDotNetStorefrontCore;
+﻿using AspDotNetStorefrontCore;
 using System;
-
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 namespace AspDotNetStorefront
 {
     /// <summary>
@@ -63,6 +68,98 @@ namespace AspDotNetStorefront
                     hdnCustomerLevel.Text = "-1";
                 }
             }
+        }
+        protected void ServerButton_Click(object sender, EventArgs e)
+        {
+
+            GetCustomerAlerts();
+            popuppanel.Visible = true;
+            Page.ClientScript.RegisterStartupScript(this.GetType(), "CallMyFunction", "preventLoad()", true);
+        }
+        private void GetCustomerAlerts()
+        {
+            try
+            {
+                using (var conn = DB.dbConn())
+                {
+                    conn.Open();
+                    using (var cmd = new SqlCommand("aspdnsf_CustomerAlertStatusSelectByCustomerIDAlertDate", conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@CustomerID", ThisCustomer.CustomerID);
+                        cmd.Parameters.AddWithValue("@CustomerLevelID", ThisCustomer.CustomerLevelID);
+                        cmd.Parameters.AddWithValue("@AlertDate", DateTime.Now);
+
+                        IDataReader idr = cmd.ExecuteReader();
+                        rptCustomerAlerts.DataSource = idr;
+                        rptCustomerAlerts.DataBind();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                SysLog.LogMessage(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.ToString() + " :: " + System.Reflection.MethodBase.GetCurrentMethod().Name,
+                ex.Message + ((ex.InnerException != null && string.IsNullOrEmpty(ex.InnerException.Message)) ? " :: " + ex.InnerException.Message : ""),
+                MessageTypeEnum.GeneralException, MessageSeverityEnum.Error);
+            }
+        }
+
+        /// <summary>
+        /// Repeater ItemCommand Event
+        /// </summary>
+        protected void rptCustomerAlerts_ItemCommand(object source, RepeaterCommandEventArgs e)
+        {
+            try
+            {
+                int customerAlertStatusID = Convert.ToInt32(e.CommandArgument);
+                const string readSP = "aspdnsf_CustomerAlertStatusRead";
+                const string deleteSP = "aspdnsf_CustomerAlertStatusDelete";
+
+                if (e.CommandName == "Delete")
+                {
+                    UpdateCustomerAlert(customerAlertStatusID, deleteSP);
+                }
+                else if (e.CommandName == "Read")
+                {
+                    UpdateCustomerAlert(customerAlertStatusID, readSP);
+                }
+                GetCustomerAlerts();
+            }
+            catch (Exception ex)
+            {
+                SysLog.LogMessage(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.ToString() + " :: " + System.Reflection.MethodBase.GetCurrentMethod().Name,
+                ex.Message + ((ex.InnerException != null && string.IsNullOrEmpty(ex.InnerException.Message)) ? " :: " + ex.InnerException.Message : ""),
+                MessageTypeEnum.GeneralException, MessageSeverityEnum.Error);
+            }
+        }
+
+        /// <summary>
+        /// Update Customer Alert
+        /// </summary>
+        /// <param name="customerAlertStatusID">customerAlertStatusID</param>
+        /// <param name="spName">spName</param>
+        private void UpdateCustomerAlert(int customerAlertStatusID, string spName)
+        {
+            try
+            {
+                using (var conn = DB.dbConn())
+                {
+                    conn.Open();
+                    using (var cmd = new SqlCommand(spName, conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@CustomerAlertStatusID", customerAlertStatusID);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                SysLog.LogMessage(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.ToString() + " :: " + System.Reflection.MethodBase.GetCurrentMethod().Name,
+                ex.Message + ((ex.InnerException != null && string.IsNullOrEmpty(ex.InnerException.Message)) ? " :: " + ex.InnerException.Message : ""),
+                MessageTypeEnum.GeneralException, MessageSeverityEnum.Error);
+            }
+
         }
 
         /// <summary>
