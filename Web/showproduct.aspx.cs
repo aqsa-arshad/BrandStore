6c9506b2d4bd278da3529e05ba5b5bd037723b44
shehriyar-zafar-confiz <shehriyar.zafar@confiz.com>
2015-11-12 19:11:28 +0500

    Resolve bug of Parent category highlighted

--- Web/showproduct.aspx.cs
+++ Web/showproduct.aspx.cs
@@ -20,6 +20,10 @@ namespace AspDotNetStorefront
 	[PageType("product")]
     public partial class showproduct : SkinBase
     {
+        String SourceEntityInstanceName = String.Empty;
+        protected string parentCategoryID = String.Empty;
+
+
         int ProductID;
         bool IsAKit;
         bool RequiresReg;
@@ -206,9 +210,7 @@ namespace AspDotNetStorefront
             ManufacturerName = ManufacturerHelper.GetEntityName(ManufacturerID, ThisCustomer.LocaleSetting);
             DistributorName = DistributorHelper.GetEntityName(DistributorID, ThisCustomer.LocaleSetting);
             GenreName = GenreHelper.GetEntityName(GenreID, ThisCustomer.LocaleSetting);
-            VectorName = VectorHelper.GetEntityName(VectorID, ThisCustomer.LocaleSetting);
-
-            String SourceEntityInstanceName = String.Empty;
+            VectorName = VectorHelper.GetEntityName(VectorID, ThisCustomer.LocaleSetting);           
 
             if (ManufacturerID != 0)
             {
@@ -419,6 +421,7 @@ namespace AspDotNetStorefront
                 }
             }
             litOutput.Text = m_PageOutput;
+            GetParentCategory();
         }
 
         /// <summary>
@@ -600,5 +603,23 @@ namespace AspDotNetStorefront
             return masterHome;
         }
 
+        private void GetParentCategory()
+        {
+            using (var conn = DB.dbConn())
+            {
+                conn.Open();
+                var query = "select ParentCategoryID from Category where Name = '" + SourceEntityInstanceName + "'" ;
+                using (var cmd = new SqlCommand(query, conn))
+                {
+                    cmd.CommandType = CommandType.Text;
+
+                    IDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        parentCategoryID = reader["ParentCategoryID"].ToString();
+                    }
+                }
+            }            
+        }
     }
 }
