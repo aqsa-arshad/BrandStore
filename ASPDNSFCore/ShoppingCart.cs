649bf7459de4c4a3a1c2d03ccf46f4de5880accb
aqsa-arshad-confiz <aqsa.arshad@confiz.com>
2016-01-30 13:09:11 +0500

    CJP-472

--- ASPDNSFCore/ShoppingCart.cs
+++ ASPDNSFCore/ShoppingCart.cs
@@ -8560,7 +8560,7 @@ namespace AspDotNetStorefrontCore
                 tmpS.Append("<input type=\"hidden\" name=\"Price_vldt\" value=\"[req][number][blankalert=" + AppLogic.GetString("shoppingcart.cs.113", SkinID, LocaleSetting) + "][invalidalert=" + AppLogic.GetString("shoppingcart.cs.114", SkinID, LocaleSetting) + "]\">\n");
             }
 
-            tmpS.Append("<p id=\"pInStock\" style=\"Display: None; \"><span class=\"black-blu-label\"><font>In Stock: </font><label class=\"blue-color\" runat=\"server\" ClientIDMode=\"Static\" id=\"lblInStock\"/></span></p>");
+            tmpS.Append("<p id=\"pInStock\" style=\"Display: None; \"><span class=\"black-blu-label\"><font>In Stock: </font><label class=\"blue-color\" runat=\"server\" ClientIDMode=\"Static\" id=\"lblInStock\"></label></span></p>");
             tmpS.Append("<p id=\"pOutofStock\" style=\"Display: None; \"><span class=\"notify \">Out of Stock</span></p>");
 
             //Colors Alternative
